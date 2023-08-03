90bf57d088c9d8dfdb6adb5f2fbeb35d01e50103
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-08-03 02:08:07 -0700

    adding about section

--- src/routes/home.js
+++ src/routes/home.js
@@ -12,6 +12,37 @@ const Home = () => {
           <small>Meal Plans Made Simple.</small>
         </div>
       </div>
+      <div className="home-about">
+        <div className="home-about-img">
+          {/* <img src={about} alt="About Image" /> */}
+        </div>
+        <div className="home-about-text">
+          <h1>ABOUT</h1>
+          <p>
+            At Remplr, we believe that nutrition is the cornerstone of a healthy
+            and fulfilling life. Our platform is dedicated to empowering
+            nutrition professionals and enthusiasts alike to create personalized
+            meal plans that cater to their clients' unique dietary needs and
+            preferences.
+          </p>
+          <p>
+            Our mission is to revolutionize the way meal planning is done by
+            providing a seamless and efficient platform for nutritionists,
+            dietitians, fitness coaches, and health enthusiasts to craft
+            expertly tailored meal plans. We strive to make the process of meal
+            planning easy, enjoyable, and accessible to all, helping our users
+            transform their client's health and well-being through the power of
+            nutrition.
+          </p>
+          <p>
+            We understand that creating meal plans can be time-consuming and
+            challenging. That's why we've developed a user-friendly interface
+            that simplifies the process, allowing nutrition professionals to
+            save valuable time and focus on what truly matters – guiding their
+            clients toward a healthier lifestyle.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
