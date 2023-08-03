1410c017c7fc0800398313dbda20e30b4973c5ea
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-08-03 04:34:24 -0700

    adding contact section to home component

--- src/routes/home.js
+++ src/routes/home.js
@@ -115,6 +115,22 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <div className="home-contact">
+        <h1>GET IN TOUCH</h1>
+        <div className="home-contact-container">
+          <div className="home-contact-socials">
+            <h3>SOCIALS</h3>
+            <div className="home-contact-social-container">
+              <small>Instagram</small>
+              <small>Twitter</small>
+              <small>Facebook</small>
+              <small>LinkedIn</small>
+            </div>
+          </div>
+          <p>HELLO@REMPLR.COM</p>
+        </div>
+      </div>
     </div>
   );
 };
